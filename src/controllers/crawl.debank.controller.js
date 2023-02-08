4bad5d9364a34b2b54fbba1813b27214505e8dec
Bruno VU <bruno.vu.ext@franctvpub.fr>
2023-02-08 13:48:55 +0700

    add swagger docs for debank whale

--- src/controllers/crawl.debank.controller.js
+++ src/controllers/crawl.debank.controller.js
@@ -5,11 +5,8 @@ const catchAsync = require('../utils/catchAsync');
 const { debankService } = require('../services');
 
 const crawlWhale = catchAsync(async (req, res) => {
-
   await debankService.crawlDebankWhale();
-
   // await debankService.createWhale(whaleData);
-
   res.send({});
 });
 
